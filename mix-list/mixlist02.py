60762ceb35a9f59847eb6f3893b36fc10d95eb93
sahilkoul29101983 <sahil.koul@citi.com>
2022-03-07 16:47:32 +0000

    mixed lists

---
+++ mix-list/mixlist02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#IPlist
+iplist = [5060,"80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+
+#Display the ip
+
+print("the first IP is : " + iplist[3])
+print("the second IP is : " + iplist[4])
+
