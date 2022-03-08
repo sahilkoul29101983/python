cc12fa845f631e4a7ef7507886c2554e43636e59
sahilkoul29101983 <sahil.koul@citi.com>
2022-03-08 13:29:22 +0000

    Learning about conditional if

---
+++ iftest/conditiona02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Get the user to input the value of Hostname object
+hostname = input("What value should we set for the hostname?")
+if hostname == "MTG":
+    print("The hostname was found to be MTG")
